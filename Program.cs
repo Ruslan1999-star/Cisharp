ba89a4f01114130f54da76cecae15b95952c0edb
Ruslan <rus.nakhmetov99@gmail.com>
2023-02-14 17:04:09 +0300

    Homework1

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Введите первое число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if(a < b){
+//     Console.WriteLine($"Maximum number from {a} and {b} is {b}");
+// }
+// else{
+//     Console.WriteLine($"Maximum number from {a} and {b} is {a}");
+// }
+ 
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+ 
+// Console.WriteLine("Input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input third number");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int max = num1;
+// if(num2 > max){
+//     max = num2;
+// }
+// if(num3 > max){
+//     max = num3;
+// }
+// Console.WriteLine($"Maximum number from {num1}, {num2}, {num3} is {max} ");
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+//  Console.WriteLine("Input number");
+//  int num = Convert.ToInt32(Console.ReadLine());
+//  int current = 2;
+//  if( num < 2){
+//     Console.WriteLine($"Where no positive even numbers between 1 and {num}");
+//  }
+//  else{
+//    Console.WriteLine($"Positive even numbers between 1 and {num} are");
+//    while(current <= num){
+//       Console.Write($" {current}");
+//       current = current + 2;
+
+//    }
+//  }
+
+
+
